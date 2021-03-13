cd4bdade626549aaa8d6e65ef762bf0de18282c6
Lukasz Raczylo <lukasz@raczylo.com>
2021-03-13 12:47:54 +0000

    Patch the flannel to utilise host-gw.

--- step3/infrastructure/modules/networking-flannel/main.tf
+++ step3/infrastructure/modules/networking-flannel/main.tf
@@ -12,3 +12,18 @@ resource "null_resource" "flannel_networking" {
     command = "kubectl delete -f https://raw.githubusercontent.com/coreos/flannel/master/Documentation/kube-flannel.yml"
   }
 }
+
+resource "null_resource" "flannel_networking_patch" {
+  depends_on = [ null_resource.flannel_networking ]
+  provisioner = "local-exec" {
+    when   = create
+    command = << EOF
+kubectl patch configmap kube-flannel-cfg -n kube-system -p '{
+  "data": {
+    "net-conf.json": "{\r\n\t\"Network\": \"10.244.0.0/16\",\r\n\t\"Backend\": { \r\n\t\t\"Type\": \"host-gw\"\n\t}\n}"
+  }
+}'"
+EOF
+  }
+
+}
