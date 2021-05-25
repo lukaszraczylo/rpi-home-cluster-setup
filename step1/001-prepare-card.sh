027962bad3f464dc60f89177a1ec307a4d26053f
alexander-hagen <github@home.hesti.com>
2021-05-25 21:25:44 +0200

    Update 001-prepare-card.sh
    Removed duplicate /dev

--- step1/001-prepare-card.sh
+++ step1/001-prepare-card.sh
@@ -11,6 +11,6 @@ ALPINE_BASE_IMAGE="https://dl-cdn.alpinelinux.org/alpine/v3.13/releases/aarch64/
 # [[ -f /Volumes/RPI0 ]] && echo "Deleting leftover directory"; rm -fr /Volumes/RPI0 || echo "Clean and dandy on the Volumes front"
 [[ ! -f static/alpine-aarch64.tar.gz ]] && wget $ALPINE_BASE_IMAGE -O static/alpine-aarch64.tar.gz || echo "Alpine baseimg already exists"
 sudo diskutil eraseDisk FAT32 RPI MBRFormat $PI_CARD
-sudo diskutil partitionDisk /dev/$PI_CARD MBRFormat FAT32 RPI0 1g FREE RPISYS 0b
+sudo diskutil partitionDisk $PI_CARD MBRFormat FAT32 RPI0 1g FREE RPISYS 0b
 sudo tar xf static/alpine-aarch64.tar.gz -C /Volumes/RPI0
 sudo cp static/headless.tar.gz /Volumes/RPI0/headless.apkovl.tar.gz
